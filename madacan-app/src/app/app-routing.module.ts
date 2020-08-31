08cece66ed9d72d4dc3a31821dff0a757710eadb
mememelm <carol.arivony@gmail.com>
2020-08-31 21:16:36 +0300

    architecture projet

--- madacan-app/src/app/app-routing.module.ts
+++ madacan-app/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './application/home/home.component';
+import { RegisterComponent } from './application/register/register.component';
+import { ConnexionComponent } from './application/connexion/connexion.component';
+import { FormationComponent } from './application/formation/formation.component';
+import { ExerciseComponent } from './application/exercise/exercise.component';
+import { TestComponent } from './application/test/test.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'formation', component: FormationComponent },
+  { path: 'exercise', component: ExerciseComponent },
+  { path: 'test', component: TestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
